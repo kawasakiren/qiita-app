97218ae54d22c3225ca8f7fd2dd6bb9f9a720bdb
河崎蓮 <kawasakiren@kawasakirennoMacBook-Pro.local>
2021-05-25 11:43:22 +0900

    認証の設定を追加

--- backend/backend/base.py
+++ backend/backend/base.py
@@ -125,3 +125,18 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',    # <-- この行を追加
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+JWT_AUTH = {
+    'JWT_SECRET_KEY': SECRET_KEY,
+}
